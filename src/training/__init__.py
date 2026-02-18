576270313208925aeb2c3f33b9ce09b077c8fcc8
Mr. Ashish Panday <ashishpanday953@gmail.com>
2026-02-18 22:08:20 +0545

    fix: import compute_metrics to create_compute_metrics_fn

--- src/training/__init__.py
+++ src/training/__init__.py
@@ -1,10 +1,10 @@
 """Training pipeline modules."""
 
 from .trainer import create_trainer, train_model
-from .metrics import compute_metrics
+from .metrics import create_compute_metrics_fn
 
 __all__ = [
     "create_trainer",
     "train_model",
-    "compute_metrics"
+    "create_compute_metrics_fn"
 ]
